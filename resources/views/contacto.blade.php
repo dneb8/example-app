42c960d3a58486830497c02a7e09a043b4981594
dneb8 <107234871+dneb8@users.noreply.github.com>
2023-09-05 16:43:04 -0600

    Formulario de contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de contacto</h1><hr>
+    <form action="">
+        <label for="correo">Correo</label><br>
+        <input type="email" name="correo"><br>
+        <label for="comentario">Comentario</label><br>
+        <textarea name="comentario" cols="30" rows="10"><br>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Agrgamos una ruta de contacto */
+Route::get('/contacto', function () {
+    return view('contacto');
+});
